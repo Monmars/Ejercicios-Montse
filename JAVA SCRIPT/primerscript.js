63576cd7134ded218a187dfffedd9952a31d9f11
Monmars <61312741+Monmars@users.noreply.github.com>
2020-02-21 12:53:57 +0100

    Add files via upload

---
+++ JAVA SCRIPT/primerscript.js
@@ -0,0 +1,7 @@
+// Al cargarse el archivo JavaScript, se muestra un mensaje
+alert("Hola mundo")
+// Despues del primer mensaje, se muestra otro mensaje seguido
+alert("Soy el primer script");
+
+var visible= true;
+alert(!visible); // Muestra "false" y no "true"
